eb7c3d42646e8097bf54414d4ae95b43be0748ce
shcrossan <shanecrossan@gmail.com>
2015-06-01 15:47:38 -1000

    add auth details to domain model

--- itieau_app/models.py
+++ itieau_app/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 class domain(models.Model):
     domain = models.CharField(max_length=120, null=False, blank=False)
     user = models.ManyToManyField(User)
+    UserAuth = models.CharField(max_length=30, null=True, blank=False)
+    PassAuth = models.CharField(max_length=30, null=True, blank=False)
 
     def __unicode__(self):
         return self.domain
