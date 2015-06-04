3ef0a1d669c2d030979b7a487a0533344d708691
shcrossan <shanecrossan@gmail.com>
2015-06-04 09:18:11 -1000

    fix wsgi

--- itieau/wsgi.py
+++ itieau/wsgi.py
@@ -13,13 +13,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "itieau.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-def django_application(environ, start_response):
-
-    if environ.get("mod_wsgi.input_chunked") == "1":
-        stream = environ["wsgi.input"]
-        data = stream.read()
-        environ["CONTENT_LENGTH"] = str(len(data))
-        environ["wsgi.input"] = StringIO.StringIO(data)
-
-    return application(environ, start_response)
