24ccc711a755aad3cc3fad4ea633b9fbb6c21ea9
shcrossan <shanecrossan@gmail.com>
2015-06-03 20:27:57 -1000

    add content length header

--- itieau_app/views.py
+++ itieau_app/views.py
@@ -12,7 +12,7 @@ import requests
 
 @login_required
 def home(request):
-    r = requests.get("http://tiapiriuf.dyndns.info/BDY_Data.htm", auth=('S500', 'ADMINI'), params='p0=101&p1=100&p2=7&p3=8&p4=45&p5=48&p6=9&p7=10&p8=94&p9=41&p10=46&p11=49&p12=25&p13=160&p14=63&p15=66&p16=28&p17=42')
+    r = requests.get("http://tiapiriuf.dyndns.info/BDY_Data.htm", auth=('S500', 'ADMINI'), params='p0=101&p1=100&p2=7&p3=8&p4=45&p5=48&p6=9&p7=10&p8=94&p9=41&p10=46&p11=49&p12=25&p13=160&p14=63&p15=66&p16=28&p17=42', headers={"Content-Length": 160})
     status_code = r.status_code
     header = r.headers
     text = r.text.encode('ASCII', 'ignore')
