dd40560088b75c9bb242fae21d41da28b5f13846
shcrossan <shanecrossan@gmail.com>
2015-06-05 10:59:16 -1000

    add requests hitting php server with curl

--- itieau_app/views.py
+++ itieau_app/views.py
@@ -9,10 +9,19 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 import requests
 
+#Lib
+import base64
+from mechanize import Browser
+
 
 @login_required
 def home(request):
-    r = requests.get("http://tiapiriuf.dyndns.info/BDY_Data.htm", auth=('S500', 'ADMINI'), params='p0=101&p1=100&p2=7&p3=8&p4=45&p5=48&p6=9&p7=10&p8=94&p9=41&p10=46&p11=49&p12=25&p13=160&p14=63&p15=66&p16=28&p17=42', headers={"Content-Length": 160})
+    # user = 'S500'
+    # pwd = 'ADMINI'
+    # url = 'http://tiapiriuf.dyndns.info/BDY_Data.htm?p0=101&p1=100&p2=7&p3=8&p4=45&p5=48&p6=9&p7=10&p8=94&p9=41&p10=46&p11=49&p12=25&p13=160&p14=63&p15=66&p16=28&p17=42'
+    # browser = Browser()
+    # browser.addheaders.append(('Authorization', 'Basic %s' % base64.encodestring('%s:%s' % (user, pwd))))
+    r = requests.get("http://107.170.192.206/test.php")
     status_code = r.status_code
     header = r.headers
     text = r.text.encode('ASCII', 'ignore')
