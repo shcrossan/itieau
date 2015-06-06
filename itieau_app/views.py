830de42d3c3eb510b039f2b77e8c237ab05c4a59
shcrossan <shanecrossan@gmail.com>
2015-06-05 16:47:38 -1000

    push production graphs

--- itieau_app/views.py
+++ itieau_app/views.py
@@ -19,16 +19,21 @@ def home(request):
     text = r.text.encode('ASCII', 'ignore')
     split_text = str.split(text, 'Bornier local')
     value = split_text[0][-5:]
-    value_round = int(round(value, 0))
+    value_str = str.strip(value, '\x1e')
+    value_round = round(float(value_str))
 
     r2 = requests.get("http://107.170.192.206/socredo.php")
     text2 = r2.text.encode('ASCII', 'ignore')
     split_text2 = str.split(text2, 'Bornier local')
     value2 = split_text2[5][-5:]
+    value_str2 = str.strip(value2, '\x1e')
+    value_round2 = round(float(value_str2))
 
     r3 = requests.get("http://107.170.192.206/schmidt.php")
     text3 = r3.text.encode('ASCII', 'ignore')
     split_text3 = str.split(text3, 'Bornier local')
     value3 = split_text3[0][-5:]
+    value_str3 = str.strip(value3, '\x1e')
+    value_round3 = round(float(value_str3))
 
     return render_to_response('home.html', locals(), context_instance=RequestContext(request))
