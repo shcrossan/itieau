4fb2ae9921d24dfcada3458140e57e3a2259e604
shcrossan <shanecrossan@gmail.com>
2015-06-10 16:16:01 -1000

    crons test contact loop

--- itieau_app/crons.py
+++ itieau_app/crons.py
@@ -22,10 +22,10 @@ class MyCronJob(CronJobBase):
         value = split_text[0][-5:]
         value_str = str.strip(value, '\x1e')
         value_float = float(value_str)
-        message = 'Water level at Tiapiri is less then 1m **test if**!'
+        message = 'Water level at Tiapiri is less then 1m **test if loop User**!'
 
         if value_float > 1:
-            contact_obj = contact.object.filter(user='faaa')
+            contact_obj = contact.object.filter(User='faaa')
             for c_ob in contact_obj:
                 number = c_ob.number
                 gateway.sendMessageToNumber(number, message, '8659')
